5dd07984e252ff353cbd6bdfc49c71996c1bab80
Keidan (K. Billonneau) <kuso.fr@gmail.com>
2013-07-15 19:02:08 +0200

    update to tk changes

--- src/mzip.c
+++ src/mzip.c
@@ -1,6 +1,6 @@
 /**
  *******************************************************************************
- * @file unzip.c
+ * @file mzip.c
  * @author Keidan
  * @date 03/02/2013
  * @par Project
@@ -23,12 +23,12 @@
 #include <getopt.h>
 #include <signal.h>
 #include <tk/text/string.h>
-#include <tk/sys/uz.h>
+#include <tk/sys/z.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 
 
-static uz_t uz = NULL;
+static z_t z = NULL;
 
 static const struct option long_options[] = { 
     { "help"   , 0, NULL, 'h' },
@@ -36,21 +36,21 @@ static const struct option long_options[] = {
     { NULL     , 0, NULL,  0  } 
 };
 
-static void unzip_sig_int(sig_t s);
-static void unzip_cleanup(void);
+static void mzip_sig_int(sig_t s);
+static void mzip_cleanup(void);
 
 /**
  * Affichage du 'usage'.
  * @param err Code passe a exit.
  */
 void usage(int err) {
-  fprintf(stdout, "usage: unzip --input [zip] options\n");
+  fprintf(stdout, "usage: mzip --input [zip] options\n");
   fprintf(stdout, "\t--help, -h: Print this help.\n");
   fprintf(stdout, "\t--input, -i: ZIP file.\n");
   exit(err);
 }
 
-void unzip_file_content(uz_t uz, struct uzentry_s entry) {
+void mzip_file_content(z_t z, struct zentry_s entry) {
   if(entry.isdir) {
     printf("Create directory: %s\n", entry.name);
     mkdir(entry.name, 0777);
@@ -64,10 +64,10 @@ void unzip_file_content(uz_t uz, struct uzentry_s entry) {
 
 int main(int argc, char** argv) {
   char filename[FILENAME_MAX];
-  fprintf(stdout, "Unzip is a FREE software.\nCopyright 2013-2013 By kei\nLicense GPL.\n\n");
+  fprintf(stdout, "Mzip is a FREE software.\nCopyright 2013-2013 By kei\nLicense GPL.\n\n");
   /* pour fermer proprement sur le kill */
-  atexit(unzip_cleanup);
-  signal(SIGINT, (__sighandler_t)unzip_sig_int);
+  atexit(mzip_cleanup);
+  signal(SIGINT, (__sighandler_t)mzip_sig_int);
 
   bzero(filename, FILENAME_MAX);
 
@@ -85,19 +85,19 @@ int main(int argc, char** argv) {
     }
   }
 
-  uz = uz_open(filename);
-  if(uz_get_contents(uz, unzip_file_content))
+  z = z_open(filename);
+  if(z_get_contents(z, mzip_file_content))
     fprintf(stderr, "Unable to get the zip fie content\n");
 
   return EXIT_SUCCESS;
 }
 
 
-static void unzip_sig_int(sig_t s) {
+static void mzip_sig_int(sig_t s) {
   printf("\n"); /* saute le ^C qui s'affiche dans la console... */
   exit(0); /* call atexit */
 }
 
-static void unzip_cleanup(void) {
-  if(uz) uz_close(uz), uz = NULL;
+static void mzip_cleanup(void) {
+  if(z) z_close(z), z = NULL;
 }
