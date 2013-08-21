e0a275fc519ba3adc0ab2ffb3c49aa01bc796ff3
Keidan (K. Billonneau) <kuso.fr@gmail.com>
2013-08-21 16:48:34 +0200

    Fix tk updates

--- cmake/tk.cmake
+++ cmake/tk.cmake
@@ -1,32 +1,40 @@
 #tk.cmake
+set(tk_prefix lib/tk/)
 
 # Build TK env
-exec_program(${CMAKE_COMMAND} lib/tk
+exec_program(${CMAKE_COMMAND} ${tk_prefix}
   ARGS
   -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DDISTRIBUTION=${DISTRIBUTION} 
 )
 # Creation of tk targets
 add_custom_target(libtk 
-  COMMAND make -C lib/tk
+  COMMAND make -C ${tk_prefix}
 )
 add_custom_target(libtk_clean-all 
-  COMMAND make -C lib/tk clean-all
+  COMMAND make -C ${tk_prefix} clean-all
 )
 
-add_dependencies(unzip libtk)
+add_dependencies(mzip libtk)
 add_dependencies(clean-all libtk_clean-all)
+
 set(extra_clean 
-  lib/tk/lib/${DISTRIBUTION}/${CMAKE_BUILD_TYPE}/libtk.so 
-  lib/tk/src/tk/CMakeFiles/tk.dir/text/string.c.o
-  lib/tk/src/tk/CMakeFiles/tk.dir/text/stringtoken.c.o
-  lib/tk/src/tk/CMakeFiles/tk.dir/sys/log.c.o
-  lib/tk/src/tk/CMakeFiles/tk.dir/sys/sysutils.c.o
-  lib/tk/src/tk/CMakeFiles/tk.dir/sys/uz.c.o
-  lib/tk/src/tk/CMakeFiles/tk.dir/sys/proc.c.o
-  lib/tk/src/tk/CMakeFiles/tk.dir/net/netutils.c.o
-  lib/tk/src/tk/CMakeFiles/tk.dir/net/netprint.c.o
-  lib/tk/src/zlib-minizip/CMakeFiles/zlib-minizip.dir/unzip.c.o
-  lib/tk/src/zlib-minizip/CMakeFiles/zlib-minizip.dir/ioapi.c.o
+  ${tk_prefix}lib/${DISTRIBUTION}/${CMAKE_BUILD_TYPE}/libtk.so 
+  ${tk_prefix}src/tk/CMakeFiles/tk.dir/text/string.c.o
+  ${tk_prefix}src/tk/CMakeFiles/tk.dir/text/stringbuffer.c.o
+  ${tk_prefix}src/tk/CMakeFiles/tk.dir/text/stringtoken.c.o
+  ${tk_prefix}src/tk/CMakeFiles/tk.dir/sys/log.c.o
+  ${tk_prefix}src/tk/CMakeFiles/tk.dir/sys/stools.c.o
+  ${tk_prefix}src/tk/CMakeFiles/tk.dir/sys/ssig.c.o
+  ${tk_prefix}src/tk/CMakeFiles/tk.dir/sys/proc.c.o
+  ${tk_prefix}src/tk/CMakeFiles/tk.dir/sys/z.c.o
+  ${tk_prefix}src/tk/CMakeFiles/tk.dir/shell/shell.c.o
+  ${tk_prefix}src/tk/CMakeFiles/tk.dir/shell/shell_term.c.o
+  ${tk_prefix}src/tk/CMakeFiles/tk.dir/io/sr.c.o
+  ${tk_prefix}src/tk/CMakeFiles/tk.dir/io/net/ntools.c.o
+  ${tk_prefix}src/tk/CMakeFiles/tk.dir/io/net/nprint.c.o
+  ${tk_prefix}src/zlib-minizip/CMakeFiles/zlib-minizip.dir/unzip.c.o
+  ${tk_prefix}src/zlib-minizip/CMakeFiles/zlib-minizip.dir/ioapi.c.o
 )
+
 set_directory_properties(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES "${extra_clean}")
 
--- src/mzip.c
+++ src/mzip.c
@@ -23,6 +23,7 @@
 #include <getopt.h>
 #include <signal.h>
 #include <tk/text/string.h>
+#include <tk/sys/ssig.h>
 #include <tk/sys/z.h>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -33,10 +34,12 @@ static z_t z = NULL;
 static const struct option long_options[] = { 
     { "help"   , 0, NULL, 'h' },
     { "input"  , 1, NULL, 'o' },
+    { "create" , 0, NULL, 'c' },
+    { "extract", 0, NULL, 'x' },
     { NULL     , 0, NULL,  0  } 
 };
 
-static void mzip_sig_int(sig_t s);
+static void mzip_signals(int s);
 static void mzip_cleanup(void);
 
 /**
@@ -44,9 +47,10 @@ static void mzip_cleanup(void);
  * @param err Code passe a exit.
  */
 void usage(int err) {
-  fprintf(stdout, "usage: mzip --input [zip] options\n");
+  fprintf(stdout, "usage: mzip --input [zip] [action]\n");
   fprintf(stdout, "\t--help, -h: Print this help.\n");
   fprintf(stdout, "\t--input, -i: ZIP file.\n");
+  fprintf(stdout, "\t--create, -c: Action mode create.\n");
   exit(err);
 }
 
@@ -63,38 +67,56 @@ void mzip_file_content(z_t z, struct zentry_s entry) {
 }
 
 int main(int argc, char** argv) {
+  _Bool create = 0;
+  _Bool extract = 0;
   char filename[FILENAME_MAX];
+
+  ssig_init(log_init_cast_user("mzip", LOG_PID|LOG_CONS|LOG_PERROR), mzip_cleanup);
+  ssig_add_signal(SIGINT, mzip_signals);
+  ssig_add_signal(SIGTERM, mzip_signals);
+
   fprintf(stdout, "Mzip is a FREE software.\nCopyright 2013-2013 By kei\nLicense GPL.\n\n");
-  /* pour fermer proprement sur le kill */
-  atexit(mzip_cleanup);
-  signal(SIGINT, (__sighandler_t)mzip_sig_int);
 
   bzero(filename, FILENAME_MAX);
 
   int opt;
-  while ((opt = getopt_long(argc, argv, "hi:", long_options, NULL)) != -1) {
+  while ((opt = getopt_long(argc, argv, "hi:cx", long_options, NULL)) != -1) {
     switch (opt) {
       case 'h': usage(0); break;
       case 'i': /* input */
 	strncpy(filename, optarg, FILENAME_MAX);
 	break;
+      case 'c':
+	create = 1;
+	break;
+      case 'x':
+	extract = 1;
+	break;
       default: /* '?' */
-	fprintf(stderr, "Unknown option '%c'\n", opt);
+	logger(LOG_ERR, "Unknown option '%c'\n", opt);
 	usage(EXIT_FAILURE);
 	break;
     }
   }
+  if(!extract && !create) {
+    logger(LOG_ERR, "Please set an action mode!\n");
+    usage(EXIT_FAILURE);
+  } else if(!extract && !create) {
+    logger(LOG_ERR, "Exctract and create are sets!\n");
+    usage(EXIT_FAILURE);
+  }
 
   z = z_open(filename);
   if(z_get_contents(z, mzip_file_content))
-    fprintf(stderr, "Unable to get the zip fie content\n");
+    logger(LOG_ERR, "Unable to get the zip fie content\n");
 
   return EXIT_SUCCESS;
 }
 
 
-static void mzip_sig_int(sig_t s) {
-  printf("\n"); /* saute le ^C qui s'affiche dans la console... */
+static void mzip_signals(int s) {
+  if(s == SIGINT)
+    printf("\n"); // for ^C
   exit(0); /* call atexit */
 }
 
